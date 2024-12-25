6622a057719a24659505b7f481ea6380420a9642
matias <matias.calvo@outlook.com>
2024-12-25 18:25:36 -0300

    comenzo el desarrollo del footer

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,9 +1,19 @@
 import React from "react";
+import '../../styles/components/layout/Footer.css'
 
 const Footer = (props) => {
     return (
         <footer>
-            <p>Derechos Reservados. Hermes Agency 2024</p>
+            
+            <div className="logos-footer">
+                <p>Encontranos en:</p>
+                <ul>
+                    <li><img src="/img/instagram-logo.png" alt="ig logo"></img></li>
+                    <li><img src="/img/linkedin-logo.webp" alt="ig logo"></img></li>
+                    <li><img src="/img/facebook-logo.png" alt="ig logo"></img></li>
+                </ul>
+            </div>
+            <p className="copyright">Derechos Reservados. Hermes Agency 2024</p>
         </footer>
     )
 }
